025eb88d75f54de1283a582fea72876be8316c7c
János Roden <janos.roden@blacksalt.io>
2018-09-30 15:02:57 +0200

    Handle empty record

--- check.sh
+++ check.sh
@@ -1,50 +1,53 @@
 #!/usr/bin/env bash
 set -e
 
+function printCurrency() {
+    LC_NUMERIC=en_US.utf8 printf "%'d Ft" "$1" | tr ',' ' '
+    # LC_NUMERIC=en_US printf "%'d Ft" "$1" | tr ',' ' '
+}
+
+[[ $# -ne 2 || ! -f $1 || ! -d $2 ]] && echo "Használat: #0 <config file> <output dir>" >&2 && exit 1
+source "$1"
+# Check myNumbers
+[[ -z $myNumbers ]] && echo "A myNumbers definiálása kötelező!" >&2 && exit 1
+[[ ${#myNumbers[@]} -ne $[(${#myNumbers[@]} / 5) * 5] ]] && echo "A myNumbers tömbben ötösével kell felsorolni a szelvényeket!" >&2 && exit 1
+for i in ${!myNumbers[@]}; do
+    [[ ${myNumbers[$i]} -le 0 || ${myNumbers[$i]} -gt 90 ]] && echo "A számoknak 1 és 90 között kell lenniük!" >&2 && exit 1
+done
+
+# Check numberOfPlayers
+[[ -z $numberOfPlayers ]] && numberOfPlayers=1
+[[ $numberOfPlayers -le 0 ]] && echo "A numberOfPlayers-nek pozitív egész számnak kell lennie, ha definiálva van!" >&2 && exit 1
+
+latest="$(wget -qO - https://bet.szerencsejatek.hu/cmsfiles/otos.csv | head -1 | tr -d '\r')"
+# latest="$(date +%Y);$(date +%W);$(date +%Y.%m.%d.);0;0 Ft;34;1 592 600 Ft;2615;22 085 Ft;72302;1 745 Ft;14;28;30;52;84"
+# latest=
+[[ -z $latest || $(awk -F ';' '{print NF}' <<< "$latest") -ne 16 ]] && exit 0 # Probably a transient error
+
+year="$(cut -d ';' -f 1 <<< "$latest")"
+week="$(cut -d ';' -f 2 <<< "$latest")"
+drawDate="$(cut -d ';' -f 3 <<< "$latest")"
+declare -A winerCounts=(
+    [5]="$(cut -d ';' -f 4 <<< "$latest")"
+    [4]="$(cut -d ';' -f 6 <<< "$latest")"
+    [3]="$(cut -d ';' -f 8 <<< "$latest")"
+    [2]="$(cut -d ';' -f 10 <<< "$latest")"
+)
+declare -A winnings=(
+    [5]="$(cut -d ';' -f 5 <<< "$latest" | tr -cd '[:digit:]')"
+    [4]="$(cut -d ';' -f 7 <<< "$latest" | tr -cd '[:digit:]')"
+    [3]="$(cut -d ';' -f 9 <<< "$latest" | tr -cd '[:digit:]')"
+    [2]="$(cut -d ';' -f 11 <<< "$latest" | tr -cd '[:digit:]')"
+)
+declare numbers=(
+    "$(cut -d ';' -f 12 <<< "$latest")"
+    "$(cut -d ';' -f 13 <<< "$latest")"
+    "$(cut -d ';' -f 14 <<< "$latest")"
+    "$(cut -d ';' -f 15 <<< "$latest")"
+    "$(cut -d ';' -f 16 <<< "$latest")"
+)
+
 {
-    function printCurrency() {
-        LC_NUMERIC=en_US.utf8 printf "%'d Ft" "$1" | tr ',' ' '
-    }
-
-    [[ $# -ne 2 || ! -f $1 || ! -d $2 ]] && echo "Használat: #0 <config file> <output dir>" >&2 && exit 1
-    source "$1"
-    # Check myNumbers
-    [[ -z $myNumbers ]] && echo "A myNumbers definiálása kötelező!" >&2 && exit 1
-    [[ ${#myNumbers[@]} -ne $[(${#myNumbers[@]} / 5) * 5] ]] && echo "A myNumbers tömbben ötösével kell felsorolni a szelvényeket!" >&2 && exit 1
-    for i in ${!myNumbers[@]}; do
-        [[ ${myNumbers[$i]} -le 0 || ${myNumbers[$i]} -gt 90 ]] && echo "A számoknak 1 és 90 között kell lenniük!" >&2 && exit 1
-    done
-
-    # Check numberOfPlayers
-    [[ -z $numberOfPlayers ]] && numberOfPlayers=1
-    [[ $numberOfPlayers -le 0 ]] && echo "A numberOfPlayers-nek pozitív egész számnak kell lennie, ha definiálva van!" >&2 && exit 1
-
-    latest="$(wget -qO - https://bet.szerencsejatek.hu/cmsfiles/otos.csv | head -1 | tr -d '\r')"
-    # latest="$(date +%Y);$(date +%W);$(date +%Y.%m.%d.);0;0 Ft;34;1 592 600 Ft;2615;22 085 Ft;72302;1 745 Ft;14;28;30;52;84"
-
-    year="$(cut -d ';' -f 1 <<< "$latest")"
-    week="$(cut -d ';' -f 2 <<< "$latest")"
-    drawDate="$(cut -d ';' -f 3 <<< "$latest")"
-    declare -A winerCounts=(
-        [5]="$(cut -d ';' -f 4 <<< "$latest")"
-        [4]="$(cut -d ';' -f 6 <<< "$latest")"
-        [3]="$(cut -d ';' -f 8 <<< "$latest")"
-        [2]="$(cut -d ';' -f 10 <<< "$latest")"
-    )
-    declare -A winnings=(
-        [5]="$(cut -d ';' -f 5 <<< "$latest" | tr -cd '[:digit:]')"
-        [4]="$(cut -d ';' -f 7 <<< "$latest" | tr -cd '[:digit:]')"
-        [3]="$(cut -d ';' -f 9 <<< "$latest" | tr -cd '[:digit:]')"
-        [2]="$(cut -d ';' -f 11 <<< "$latest" | tr -cd '[:digit:]')"
-    )
-    declare numbers=(
-        "$(cut -d ';' -f 12 <<< "$latest")"
-        "$(cut -d ';' -f 13 <<< "$latest")"
-        "$(cut -d ';' -f 14 <<< "$latest")"
-        "$(cut -d ';' -f 15 <<< "$latest")"
-        "$(cut -d ';' -f 16 <<< "$latest")"
-    )
-
     # Print draw stats
     cat << EOF
 
